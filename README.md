2899f9f3d06bc5bc84afe35207b46ea1a7300e6b
Peter Pan <Peter.Pan@emc.com>
2017-01-13 22:45:21 -0500

    169.254 solution 2

--- README.md
+++ README.md
@@ -2,10 +2,20 @@
 
 Steps
 ------
-
+Option #1:  User use keyboard monitor to login any of the nodes, and input available IP
+------
 * when system boot up , auto run the script ```sudo ./SetIP_VxRail.sh &``` in background
 * waiting a while, ensure all nodes runs (recommended boot up + script run + 20s )
 * customer goes into any of the node via KVM, run ```./User_Input_IP.sh```, key in the IPs one by one, after prompt.then hit ``CTRL+D``` to complete the key in.
 * the node who customer goes to, will set IP from the pool
 * the other nodes will select IP from the pool later
+
+------
+Option #2: If 169.254.0.0/16 is safe in end-user enviroment
+------
+* when system boot up , auto run the script ```sudo ./SetIP_VxRail2.sh ``` 
+* the nodes will select IP from the pool , among the IP ranges
+
+
+
 
---
+++ SetIP_VxRail2.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+rm /tmp/mac_list.txt
+rm /tmp/mac_list_ordered.txt
+cp  169_254_IP_List.txt /tmp/IP_Pool_Input.txt
+#rm /tmp/IP_Pool_Input.txt
+
+NIC=eth1
+
+VXRAIL_FLAG="VxRail Node."
+
+sudo service lldpd start
+#sudo service lldpd stop 
+#sudo pkill lldpd 
+#sudo lldpd -d -I $NIC -S $VXRAIL_FLAG &
+echo "[Info] waiting lldpd to working. please wait 20 sec"
+sleep 5
+#sleep 20
+#echo "[Info]sleep done. "
+
+#if [ ! $(type "lldpd" > /dev/null) ] #|| [ ! type "lldpcli" > /dev/null ];
+if [ "$(which lldpd)" == "" ] || [ "$(which lldpcli)" == "" ]
+then
+    echo "[Error]: please pre-install lldpd in VxRail nodes. aborting"
+    exit 1
+fi
+
+
+PREFIX=$VXRAIL_FLAG"User Define IP Pool:"
+
+#############################
+#  Assuming the node already obtinaed IP pool, will set its desc to $PREFIX IP1|IP2|IP3....
+#############################
+while true; do
+
+
+    # this /tmp/IP_Pool_Input.txt comes from User Input
+    if [ -f /tmp/IP_Pool_Input.txt ]
+    then
+        IP_LIST=$(cat /tmp/IP_Pool_Input.txt)
+        echo "[Info]Detect Customer Input of IP Pools."
+        IPs=$(echo $IP_LIST| tr "|" "\n")
+        break;
+    fi
+
+    DESC=$(lldpcli  show neighbors | grep "$PREFIX" )
+    # Get VxRail IP List
+    IP_LIST=${DESC#* $PREFIX}
+    if [  "$(echo $IP_LIST)" != ""  ] 
+    then
+        echo IP_LIST= $IP_LIST
+        echo "[Info]Another VxRail node obtains IP pool input.Start to grab IP from it...."
+        break;
+    fi
+
+    sleep 3
+
+done
+
+
+
+# split
+IPs=$(echo $IP_LIST| tr "|" "\n") 
+
+IP_COUNT=${#IPs[@]}
+#sort
+IPs_sorted=$(for l in ${IPs[@]}; do echo $l; done | sort)
+
+
+#######################################################
+#
+# Assume all VxRail nodes are up and running to here.
+#
+######################################################
+# get all vXrail MAC List
+lldpcli  show neighbors |grep VxRail -B2|grep ChassisID |awk '{print $NF}' > /tmp/mac_list.txt
+# get all vXrail IP List
+lldpcli  show neighbors |grep VxRail -A1|grep MgmtIP |awk '{print $NF}' > /tmp/used_ip_list.txt
+
+
+
+# get local MAC, assuming $NIC
+MY_MAC=$( ifconfig $NIC      |grep HWaddr|awk '{print $NF}' )
+
+
+
+MAC_COUNT=$(wc -l /tmp/mac_list.txt | awk '{print $1}' )
+
+
+#http://unix.stackexchange.com/questions/242251/count-number-of-elements-in-bash-array-where-the-name-of-the-array-is-dynamic
+
+if [ "${MAC_COUNT}" -gt "$IP_COUNT" ]
+then
+    echo "[Warning]: User Input IP Pool is Not enough! aborting"
+fi
+
+
+# sort MAC
+echo $MY_MAC >> /tmp/mac_list.txt
+sort /tmp/mac_list.txt > /tmp/mac_list_ordered.txt
+
+
+
+# Get rank
+RANK=$( grep -n $MY_MAC /tmp/mac_list_ordered.txt |awk -F ":" '{print $1}')
+
+echo "RANK="$RANK
+
+# local IP to be configued ======== WTF, this line doesn't work in bash ??!!
+#MY_IP=${IPs_sorted[$RANK-1]}
+
+cnt=1
+for l in ${IPs_sorted[@]};
+do
+    if [ "$cnt" -gt "$RANK" ];
+    then
+        if [ "$(grep $l /tmp/used_ip_list.txt)" == "" ];
+        then
+              MY_IP=$l
+              break
+        else
+            echo "[info: $l is occupied, try next..."
+        fi
+    fi
+    let "cnt=cnt+1"
+done 
+
+if [ "$MY_IP"  == "" ]
+then
+    echo "[Error]: Not IP avaiable for this node :-(  . exit "
+    exit -1
+fi
+echo "My IP Should be : $MY_IP ( run : sudo ifconfig $NIC $MY_IP  )"
+
+sudo  ifconfig $NIC $MY_IP
